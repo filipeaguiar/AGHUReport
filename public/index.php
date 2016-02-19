4b5b32dd628aff532d3e278bc02009d0c07f0244
Filipe Aguiar <filipe_aac@yahoo.com.br>
2016-02-19 15:47:22 -0300

    Instalação de Illuminate/Database
    Remoção de arquivos de configurações php
    Criação de arquivo de configuração .env
    Criação de arquivo exclusivo de rotas

--- public/index.php
+++ public/index.php
@@ -1,40 +1,13 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
+$dotenv->load();
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 $app = new \Slim\App();
 
-$app->get('/', function () {
-  echo "Hello World";
-});
-
-$app->get('/indicadores', 'getIndicadores');
-
-$app->get('/info', function(){
-  return phpinfo();
-});
+require __DIR__ . '/../src/routes.php';
 
 $app->run();
-
-
-function getIndicadores() {
-  echo "<h1>Indicadores</h1>";
-  $sql = "SELECT * FROM ain_indicadores_hospitalares";
-  try {
-    $db = getConnection();
-    $stmt = $db->query($sql);
-    $indicadores = $stmt->fetchAll(PDO::FETCH_OBJ);
-  }
-  catch(PDOException $e) {
-    echo '"Erro":{"text":'. $e->getMessage() .'}}';
-  }
-}
-
-function getConnection() {
-  require_once( "config.php" );
-  $dbh = new PDO("pgsql:host=$dbhost;port=$dbport;dbname=$dbname", $dbuser, $dbpass); //config.php
-  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  return $dbh;
-}

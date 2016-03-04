0d6ddaa96508fb0a8879a4bd2877fe5a55ceb8e8
Filipe Aguiar <filipe_aac@yahoo.com.br>
2016-03-04 01:01:15 -0300

    Save Chart to PNG

--- public/js/app.js
+++ public/js/app.js
@@ -12,6 +12,12 @@ angular.module('aghureport').controller('TaxaDePermanenciaController', function 
   .error(function (data, status) {
     $scope.error = 'Erro' + data;
   });
+
+  $scope.saveImage = function () {
+    // jQuery('permanencia').child('svg').attr('id', 'permanenciaChart');
+
+    saveSvgAsPng(document.getElementById('permanencia'), 'Taxa_de_Permanencia.png', { backgroundColor:'#FFFFFF', scale: 2 });
+  };
 });
 
 angular.module('aghureport').controller('TaxaDeMortalidadeController', function ($scope, $http) {
@@ -26,3 +32,7 @@ angular.module('aghureport').controller('TaxaDeMortalidadeController', function 
     $scope.error = 'Erro' + data;
   });
 });
+
+function saveChart(id) {
+  saveSvgAsPng(document.getElementById(id), 'diagram.png');
+}

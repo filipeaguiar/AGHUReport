2760c512e169bb4a92312d0497af1b3ae22ede8c
Filipe Aguiar <filipe_aac@yahoo.com.br>
2016-03-07 18:20:23 -0300

    Tentative Graph save

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,2 @@
 /* global angular */
 var aghureport = angular.module('aghureport', ['angular.morris-chart']);
-
-$(document).ready(function() {
-    jQuery( "#permanencia" ).children().prop('id', 'permanenciaChart');
- });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,39 +1,38 @@
-angular.module('aghureport').controller('TaxaDePermanenciaController', function ($scope, $http) {
-  $http.get('/indicador/G/2015-03-01/2016-03-01').success(function (data) {
-    $scope.ocupacao = data;
-    $scope.xaxis = 'competencia_internacao';
-    $scope.yaxis = ['taxa_ocupacao'];
-    $scope.label = ['Taxa de Permanência'];
-    $scope.cardlabel = 'Taxa de Permanência';
-    jQuery( "#permanencia" ).children().prop('id', 'permanenciaChart');
-    console.log($scope.trocou);
+angular.module('aghureport')
+  .controller('TaxaDePermanenciaController', function ($scope, $http) {
+      $http.get('/indicador/G/2015-03-01/2016-03-01')
+            .success(function (data) {
+              $scope.ocupacao = data;
+              $scope.xaxis = 'competencia_internacao';
+              $scope.yaxis = ['taxa_ocupacao'];
+              $scope.label = ['Taxa de Permanência'];
+              $scope.cardlabel = 'Taxa de Permanência';
+            })
+            .error(function (data, status) {
+              $scope.error = 'Erro' + data;
+            });
 
-  })
-  .error(function (data, status) {
-    $scope.error = 'Erro' + data;
-  });
+      $scope.html = '';
+      $scope.saveImage = function () {
+          el = document.querySelectorAll('#permanencia svg').item(0);
+          saveSvgAsPng(el, 'Taxa_de_Permanencia.png', {
+              backgroundColor: '#FFFFFF',
+              scale: 2,
+            });
+        };
+    });
 
-  $scope.html = '';
-
-  $scope.saveImage = function () {
-     jQuery( "#permanencia" ).children().prop('id', 'permanenciaChart');
-    // saveSvgAsPng( 'permanenciaChart', 'Taxa_de_Permanencia.png', { backgroundColor:'#FFFFFF', scale: 2 });
-  };
-});
-
-angular.module('aghureport').controller('TaxaDeMortalidadeController', function ($scope, $http) {
-  $http.get('/indicador/G/2015-03-01/2016-03-01').success(function (data) {
-    $scope.mortalidade = data;
-    $scope.xaxis = 'competencia_internacao';
-    $scope.yaxis = ['taxa_mortalidade'];
-    $scope.label = ['Taxa de Mortalidade'];
-    $scope.cardlabel = 'Taxa de Mortalidade';
-  })
-  .error(function (data, status) {
-    $scope.error = 'Erro' + data;
-  });
-});
-
-function saveChart(id) {
-  saveSvgAsPng(document.getElementById(id), 'diagram.png');
-}
+angular.module('aghureport')
+    .controller('TaxaDeMortalidadeController', function ($scope, $http) {
+      $http.get('/indicador/G/2015-03-01/2016-03-01')
+            .success(function (data) {
+              $scope.mortalidade = data;
+              $scope.xaxis = 'competencia_internacao';
+              $scope.yaxis = ['taxa_mortalidade'];
+              $scope.label = ['Taxa de Mortalidade'];
+              $scope.cardlabel = 'Taxa de Mortalidade';
+            })
+            .error(function (data, status) {
+              $scope.error = 'Erro' + data;
+            });
+    });

9acc90c1b44c27d7dc2622606d3acb69c5f77c66
Filipe Aguiar <filipe_aac@yahoo.com.br>
2016-04-13 16:04:20 -0300

    Angular Route Especialidades created
    And respective files/alterations.

--- public/js/routes.js
+++ public/js/routes.js
@@ -3,6 +3,9 @@ angular.module('aghureport').config( function ($routeProvider) {
     when('/home', {
       templateUrl: "templates/home.html"
     }).
+    when('/especialidades',{
+      templateUrl: "templates/especialidades.html"
+    }).
     otherwise({
       redirectTo: '/home'
     });
